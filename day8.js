e4e2f91f98494b062a98021c8d166a11b557d6fd
SouravMil <souravmilalts@gmail.com>
2026-01-27 13:05:07 +0530

    latest-js

--- day8.js
+++ day8.js
@@ -68,6 +68,75 @@ const inpStr = 'Apple'
 function highestAlphabeticalOrder(inpStr)
 {
     const lowerStr = inpStr.toLowerCase();
-    
+    let outHighest = lowerStr[0];
+    for(let i=1;i<lowerStr.length;i++)
+    {
+        if(outHighest<lowerStr[i])
+        {
+            outHighest = lowerStr[i];
+        }
+    }
+    return outHighest;
 }
 console.log(highestAlphabeticalOrder(inpStr));
+
+//Given an integer array nums, return a new array containing only the numbers that appear exactly once in nums.
+const numInp = [1,2,2,3,4,4,5];
+function onlyUniqueNumb(numInp)
+{
+    let outputArr = [];
+    for(let i=0;i<numInp.length;i++)
+    {
+        let isUnique = true;
+        for(let j=0;j<numInp.length;j++)
+        {
+            if(i!==j && numInp[i] === numInp[j])
+            {
+                isUnique = false;
+                break;
+            }
+        }
+        if(isUnique) outputArr.push(numInp[i]);
+    }
+    return outputArr;
+}
+console.log(onlyUniqueNumb(numInp));
+
+//Given an integer n, return the product of all digits of n.
+const n=1123;
+function productOfDigits(n)
+{
+    let product = 1;
+    if(n === 0) return 0;
+    while(n>0)
+    {
+        let digit = n%10;
+        product = product*digit;
+        n=Math.floor(n/10);
+    }
+    return product;
+}
+console.log(productOfDigits(n));
+
+//Given a string s, return the character that appears the most number of times.
+const str = "Mississippi"
+function mostFrequentChar(str)
+{
+    let charFreq = {};
+    for(let char of str)
+    {
+        charFreq[char] = (charFreq[char]||0)+1;
+    }
+    let mostFrequentChar = str[0];
+    let maxFreq = 0;
+    for(let char in charFreq)
+    {
+        if(charFreq[char]>maxFreq)
+        {
+            maxFreq = charFreq[char];
+            mostFrequentChar = char;
+        }
+    }
+    return mostFrequentChar;
+}
+console.log(mostFrequentChar(str));
