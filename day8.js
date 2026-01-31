8d8644637d301c15c601f936604d46fa0cba6949
SouravMil <souravmilalts@gmail.com>
2026-01-31 09:42:33 +0530

    latest-js

--- day8.js
+++ day8.js
@@ -1,20 +1,16 @@
 //Given a string s, return the number of characters that appear more than once.
 //Ignore case.
 //Characters must be counted only once even if they repeat many times.
-const S = 'Programming';
-function multiFreqCharacters(S)
-{
+const S = "Programming";
+function multiFreqCharacters(S) {
   const newS = S.toLowerCase();
   let freq = {};
-  for(let char of newS)
-  {
-    freq[char] = (freq[char]||0)+1
+  for (let char of newS) {
+    freq[char] = (freq[char] || 0) + 1;
   }
   let outPutCount = 0;
-  for(let char in freq)
-  {
-    if(freq[char]>1)
-    {
+  for (let char in freq) {
+    if (freq[char] > 1) {
       outPutCount++;
     }
   }
@@ -25,119 +21,99 @@ console.log(multiFreqCharacters(S));
 //Given an integer n, return the number of divisors of n.
 ///logic: a variable divisor, increase it till divisor<=n
 ///if n%div === 0 then increase count
-const M=12;
-function divisorsCount(M)
-{
-    let divCounter=0;
-    for(let div=1;div<=M;div++)
-    {
-        if(M%div === 0)
-        {
-            divCounter++;
-        }
+const M = 12;
+function divisorsCount(M) {
+  let divCounter = 0;
+  for (let div = 1; div <= M; div++) {
+    if (M % div === 0) {
+      divCounter++;
     }
-    return divCounter;
+  }
+  return divCounter;
 }
 console.log(divisorsCount(M));
 
 //Given a string s, return true if it contains all unique characters (no character repeats). Otherwise return false.
-const s = 'Apple'
-function uniqueCharValidation(s)
-{
-    const newStr = s.toLowerCase();
-    let freq = {};
-    for(let char of newStr)
-    {
-        freq[char] = (freq[char]||0)+1;
+const s = "Apple";
+function uniqueCharValidation(s) {
+  const newStr = s.toLowerCase();
+  let freq = {};
+  for (let char of newStr) {
+    freq[char] = (freq[char] || 0) + 1;
+  }
+  let isUnique = true;
+  for (let char in freq) {
+    if (freq[char] > 1) {
+      isUnique = false;
+      break;
     }
-    let isUnique = true;
-    for(let char in freq)
-    {
-        if(freq[char]>1)
-        {
-            isUnique = false;
-            break;
-        }
-    }
-    return isUnique;
+  }
+  return isUnique;
 }
 console.log(uniqueCharValidation(s));
 
 //Given a string s, return the character with the highest alphabetical order (case-insensitive).
-const inpStr = 'Apple'
-function highestAlphabeticalOrder(inpStr)
-{
-    const lowerStr = inpStr.toLowerCase();
-    let outHighest = lowerStr[0];
-    for(let i=1;i<lowerStr.length;i++)
-    {
-        if(outHighest<lowerStr[i])
-        {
-            outHighest = lowerStr[i];
-        }
+const inpStr = "Apple";
+function highestAlphabeticalOrder(inpStr) {
+  const lowerStr = inpStr.toLowerCase();
+  let outHighest = lowerStr[0];
+  for (let i = 1; i < lowerStr.length; i++) {
+    if (outHighest < lowerStr[i]) {
+      outHighest = lowerStr[i];
     }
-    return outHighest;
+  }
+  return outHighest;
 }
 console.log(highestAlphabeticalOrder(inpStr));
 
 //Given an integer array nums, return a new array containing only the numbers that appear exactly once in nums.
-const numInp = [1,2,2,3,4,4,5];
-function onlyUniqueNumb(numInp)
-{
-    let outputArr = [];
-    for(let i=0;i<numInp.length;i++)
-    {
-        let isUnique = true;
-        for(let j=0;j<numInp.length;j++)
-        {
-            if(i!==j && numInp[i] === numInp[j])
-            {
-                isUnique = false;
-                break;
-            }
-        }
-        if(isUnique) outputArr.push(numInp[i]);
+const numInp = [1, 2, 2, 3, 4, 4, 5];
+function onlyUniqueNumb(numInp) {
+  let outputArr = [];
+  for (let i = 0; i < numInp.length; i++) {
+    let isUnique = true;
+    for (let j = 0; j < numInp.length; j++) {
+      if (i !== j && numInp[i] === numInp[j]) {
+        isUnique = false;
+        break;
+      }
     }
-    return outputArr;
+    if (isUnique) outputArr.push(numInp[i]);
+  }
+  return outputArr;
 }
 console.log(onlyUniqueNumb(numInp));
 
 //Given an integer n, return the product of all digits of n.
-const n=1123;
-function productOfDigits(n)
-{
-    let product = 1;
-    if(n === 0) return 0;
-    while(n>0)
-    {
-        let digit = n%10;
-        product = product*digit;
-        n=Math.floor(n/10);
-    }
-    return product;
+const n = 1123;
+function productOfDigits(n) {
+  let product = 1;
+  if (n === 0) return 0;
+  while (n > 0) {
+    let digit = n % 10;
+    product = product * digit;
+    n = Math.floor(n / 10);
+  }
+  return product;
 }
 console.log(productOfDigits(n));
 
 //Given a string s, return the character that appears the most number of times.
-const str = "Mississippi"
-function mostFrequentChar(str)
-{
-    let charFreq = {};
-    for(let char of str)
-    {
-        charFreq[char] = (charFreq[char]||0)+1;
+const str = "Mississippi";
+function mostFrequentChar(str) {
+  let charFreq = {};
+  for (let char of str) {
+    charFreq[char] = (charFreq[char] || 0) + 1;
+  }
+  let mostFrequentChar = str[0];
+  let maxFreq = 0;
+  for (let char in charFreq) {
+    if (charFreq[char] > maxFreq) {
+      maxFreq = charFreq[char];
+      mostFrequentChar = char;
     }
-    let mostFrequentChar = str[0];
-    let maxFreq = 0;
-    for(let char in charFreq)
-    {
-        if(charFreq[char]>maxFreq)
-        {
-            maxFreq = charFreq[char];
-            mostFrequentChar = char;
-        }
-    }
-    return mostFrequentChar;
+  }
+  return mostFrequentChar;
 }
 console.log(mostFrequentChar(str));
 
@@ -146,62 +122,162 @@ console.log(mostFrequentChar(str));
 // A number is called an Armstrong number if:
 // Sum of (each digit raised to the power of number of digits) = original number
 const int = 153;
-function validateArmstrongNumber(int)
-{
-    let num = int;
-    let outPut = 0;
-    const digitCount = int.toString().length;
-    while(num>0)
-    {
-        let digit = num%10;
-        outPut = outPut+Math.pow(digit,digitCount);
-        num=Math.floor(num/10);
-    }
-    return outPut === int ;
+function validateArmstrongNumber(int) {
+  let num = int;
+  let outPut = 0;
+  const digitCount = int.toString().length;
+  while (num > 0) {
+    let digit = num % 10;
+    outPut = outPut + Math.pow(digit, digitCount);
+    num = Math.floor(num / 10);
+  }
+  return outPut === int;
 }
 console.log(validateArmstrongNumber(int));
 
 //Given a string s, return a new string with the characters in alternating case.
-const string = 'hello world'
-function charsAlternateCase(string)
-{
-    const charArr = string.split('')
-    let outArr = [];
-    let count = 0;
-    for(let i=0;i<charArr.length;i++)
-    {
-        if(charArr[i] === ' ')
-        {
-            outArr.push(' ');
-            continue;
-        }
-        if(count%2 === 0)
-        {
-            outArr.push(charArr[i].toLowerCase());
-        }
-        else{
-            outArr.push(charArr[i].toUpperCase());
-        }
-        count++;
+const string = "hello world";
+function charsAlternateCase(string) {
+  const charArr = string.split("");
+  let outArr = [];
+  let count = 0;
+  for (let i = 0; i < charArr.length; i++) {
+    if (charArr[i] === " ") {
+      outArr.push(" ");
+      continue;
     }
-    return outArr.join('');
+    if (count % 2 === 0) {
+      outArr.push(charArr[i].toLowerCase());
+    } else {
+      outArr.push(charArr[i].toUpperCase());
+    }
+    count++;
+  }
+  return outArr.join("");
 }
 console.log(charsAlternateCase(string));
 
 //Given an integer n, return the sum of all even digits of n.
 const inp = 48291;
-function evenDigitsSum(inp)
-{
-    let evenSum = 0;
-    while(inp>0)
-    {
-        let digit = inp%10;
-        if(digit%2 === 0)
-        {
-            evenSum += digit;
-        }
-        inp=Math.floor(inp/10);
+function evenDigitsSum(inp) {
+  let evenSum = 0;
+  while (inp > 0) {
+    let digit = inp % 10;
+    if (digit % 2 === 0) {
+      evenSum += digit;
     }
-    return evenSum;
+    inp = Math.floor(inp / 10);
+  }
+  return evenSum;
 }
 console.log(evenDigitsSum(inp));
+
+//Given an integer array arr, return true if the array is a mountain array, otherwise return false.
+///logic: pick the largest value in the loop and make sure it is not positioned at beginning or ending of array
+/// Loop through the array and make sure it increases till largest value and then decreases.
+const intArr = [1, 2, 3, 1];
+function validationMountainArr(intArr) {
+  if (intArr.length < 3) return false;
+  let index = 0;
+  //going up towards large
+  while (index < intArr.length - 1 && intArr[index] < intArr[index + 1]) {
+    index++;
+  }
+  if (index === 0 || index === intArr.length - 1) {
+    return false;
+  }
+  while (index < intArr.length - 1) {
+    if (intArr[index] <= intArr[index + 1]) {
+      return false;
+    }
+    index++;
+  }
+  return true;
+}
+console.log(validationMountainArr(intArr));
+
+//Given an integer array, return the index of the first peak element.
+const integer = [1, 2, 4, 1, 3];
+function firstPeak(integer) {
+  if (integer.length < 3) return -1;
+  let index = 0;
+  while (index < integer.length - 1 && integer[index] < integer[index + 1]) {
+    index++;
+  }
+  if (index === 0 || index === integer.length - 1) {
+    return -1;
+  }
+
+  if (
+    integer[index] > integer[index - 1] &&
+    integer[index] > integer[index + 1]
+  ) {
+    return index;
+  }
+  return -1;
+}
+console.log(firstPeak(integer));
+
+//Given a string s, return true if it is an isogram, otherwise return false.
+const Str = "Thumbs Up";
+function validateIsogram(Str) {
+  const charArr = Str.toLowerCase().split("");
+  let seen = {};
+  for (let char of charArr) {
+    if (char === " ") continue;
+    seen[char] = (seen[char] || 0) + 1;
+  }
+  let isIsogram = true;
+  for (let char in seen) {
+    if (seen[char] > 1) {
+      isIsogram = false;
+      break;
+    }
+  }
+  return isIsogram;
+}
+console.log(validateIsogram(Str));
+
+//Given an integer array nums, return true if the array is strictly increasing, otherwise return false.
+const arrInt = [1, 3, 4, 5, 7];
+function validateIncrease(arrInt) {
+  for (let i = 0; i < arrInt.length - 1; i++) {
+    if (arrInt[i] >= arrInt[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(validateIncrease(arrInt));
+
+//Given a string s, return true if the string contains at least one vowel, otherwise return false.
+const exStr = "WHY";
+const vowel = "aeiou";
+function validateVowelPresence(exStr) {
+  const newStr = exStr.toLowerCase();
+  for (let char of newStr) {
+    if (vowel.includes(char)) {
+      return true;
+    }
+  }
+  return false;
+}
+console.log(validateVowelPresence(exStr));
+
+//Given an integer array nums, return the second largest number in the array.
+//If there is no such number, return null.
+const arrNum = [2, 3, 5, 4, 1];
+function secondLargest(arrNum) {
+  let largest = -Infinity;
+  let secLargest = -Infinity;
+  for (let i = 0; i < arrNum.length; i++) {
+    if (arrNum[i] > largest) {
+      secLargest = largest;
+      largest = arrNum[i];
+    } else if (arrNum[i] > secLargest && arrNum[i] < largest) {
+      secLargest = arrNum[i];
+    }
+  }
+  return secLargest === -Infinity ? null : secLargest;
+}
+console.log(secondLargest(arrNum));
